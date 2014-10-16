8097ecd893dba7bafb4e9f019d41ee8f558fb91f
Francisco Javier Pérez Ferrer <javiferrer@bichin>
2014-10-16 21:31:30 +0200

    Minor changes

--- init.rb
+++ init.rb
@@ -6,13 +6,13 @@ require 'queries_helper_patch'
 require 'journal_patch'
 require 'pdf_patch'
 
-Redmine::Plugin.register :redmine_hide_estimated_hours do
-  name 'Hide estimated hours'
-  author 'Dominique Lederer (return1)'
-  description 'This Redmine plugin reuses the "view_time_entries" permission to hide the estimated hours field'
-  version '1.0.6'
+Redmine::Plugin.register :redmine_hide_estimated_hours_permission do
+  name 'Hide estimated hours permission'
+  author 'Francisco Javier Perez Ferrer (based on Dominique Lederer plugin)'
+  description 'This Redmine plugin add a new permission called hide estimated hours that allow admin to hide estimated time to specific roles'
+  version '0.0.1'
   url 'https://github.com/javiferrer/redmine_hide_estimated_hours'
-  author_url 'http://twitter.com/javifferer'
+  author_url 'http://twitter.com/javiferrer'
 
   requires_redmine :version_or_higher => '2.5.0'
 
